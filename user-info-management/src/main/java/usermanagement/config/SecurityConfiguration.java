64a7250b08eb7cad11b745484c1873a13b12f47a
Forest1027 <azureforestryan@gmail.com>
2021-06-16 08:04:16 +0100

    set up security

--- user-info-management/src/main/java/usermanagement/config/SecurityConfiguration.java
+++ user-info-management/src/main/java/usermanagement/config/SecurityConfiguration.java
@@ -1,6 +1,10 @@
 package usermanagement.config;
 
+import com.okta.spring.boot.oauth.Okta;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -9,10 +13,16 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.antMatcher("/**").authorizeRequests()
-                .antMatchers("/", "/login**").permitAll()
+        http.csrf().disable().authorizeRequests()
+                .antMatchers(HttpMethod.POST,"/userManagement/v1/users/register").permitAll()
                 .anyRequest().authenticated()
                 .and()
-                .oauth2Login();
+                .oauth2ResourceServer().jwt(); //or .opaqueToken();
+
+        // process CORS annotations
+        http.cors();
+
+        // force a non-empty response body for 401's to make the response more browser friendly
+        Okta.configureResourceServer401ResponseBody(http);
     }
 }
--- user-info-management/src/main/java/usermanagement/rest/UserInfoController.java
+++ user-info-management/src/main/java/usermanagement/rest/UserInfoController.java
@@ -11,11 +11,12 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@RequestMapping("/userManagement")
 public class UserInfoController {
     @Autowired
     private UserInfoService userInfoService;
 
-    @PutMapping("/users/updateEmailVerified")
+    @PutMapping("/v1/users/updateEmailVerified")
     public void updateEmailVerified(@RequestBody UserInfo userInfo) {
         if (userInfo.getEmailVerified() == null || userInfo.getId() == null) {
             throw new RuntimeException("emailVerified or id cannot be null");
@@ -23,38 +24,33 @@ public class UserInfoController {
         userInfoService.updateEmailVerified(userInfo.getId(), userInfo.getEmailVerified());
     }
 
-    @GetMapping("/users/{userId}")
+    @GetMapping("/v1/users/{userId}")
     public Optional<UserInfo> getUserInfoById(@PathVariable int userId) {
         return userInfoService.findUserInfoById(userId);
     }
 
-    @PutMapping("/users/{userId}")
+    @PutMapping("/v1/users/{userId}")
     public UserInfo updateUserInfo(@PathVariable int userId, @RequestBody UserInfo userInfo) {
         userInfo.setId(userId);
         return userInfoService.updateUserInfo(userInfo);
     }
 
-    @DeleteMapping("/users/{userId}")
+    @DeleteMapping("/v1/users/{userId}")
     public String deleteUserInfoById(@PathVariable int userId) {
         userInfoService.deleteUserInfoById(userId);
         return "Delete successfully";
     }
 
-    @PostMapping("/users")
+    @PostMapping("/v1/users/register")
     public UserInfo createUserInfo(@RequestBody UserInfo userInfo) {
         userInfo.setId(null);
         userInfo.setEmailVerified(false);
         return userInfoService.createUserInfo(userInfo);
     }
 
-    @GetMapping("/users")
+    @GetMapping("/v1/users")
     public List<UserInfo> getUsers() {
         return userInfoService.getUsers();
-    }
-
-    @GetMapping("/hello")
-    String helloUser(@AuthenticationPrincipal OidcUser user) {
-        return "Hello " + user.getAttributes().get("email");
     }
 
 }
