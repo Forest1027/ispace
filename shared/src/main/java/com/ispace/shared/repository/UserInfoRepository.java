37c7532c26847914b42741cb02234d9515492e85
Forest1027 <azureforestryan@gmail.com>
2021-07-11 16:35:42 +0100

    adding unit testing for repository layer

--- shared/src/main/java/com/ispace/shared/repository/UserInfoRepository.java
+++ shared/src/main/java/com/ispace/shared/repository/UserInfoRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(path = "users")
-public interface UserInfoRepository extends JpaRepository<UserInfo, Integer>, UserInfoCustomRepository {
+public interface UserInfoRepository extends JpaRepository<UserInfo, String>, UserInfoCustomRepository {
 }
